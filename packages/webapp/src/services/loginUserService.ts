ecb7336b24e52e96041f98a8352dd0a1239ff81b
Matt Erhart <merhart@Matts-MacBook-Pro.local>
2021-02-16 12:24:01 -0800

    Bring back env and remove json config files, sorry for the confusion

--- packages/webapp/src/services/loginUserService.ts
+++ packages/webapp/src/services/loginUserService.ts
@@ -6,13 +6,14 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 import { AppState } from '../store/schema/AppState'
 
-const firebaseConfig = require('../configs/firebase.json')
-
 export const loginUserService = async (): Promise<AppState | undefined> => {
 	let authResp = undefined
 
 	if (!firebase.apps.length) {
-		firebase.initializeApp(firebaseConfig)
+		firebase.initializeApp({
+			apiKey: process.env.REACT_APP_FIREBASE_KEY,
+			authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+		})
 	}
 
 	const provider = new firebase.auth.GithubAuthProvider()
