37ce119a22b099395c2d4d02e8e2da7da1dbfd9f
Derek Worthen <deworthe@microsoft.com>
2021-02-17 14:30:51 -0800

    fix linting errors

---
+++ packages/healthbotapp/public/index.js
@@ -0,0 +1,158 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+
+const defaultLocale = 'en-US';
+
+const params = new URLSearchParams(location.search);
+
+function requestChatBot(loc) {
+    const oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", initBotConversation);
+    var path = "/chatBot?locale=" + extractLocale(params.get('locale'));
+
+    if (loc) {
+        path += "&lat=" + loc.lat + "&long=" + loc.long;
+    }
+    if (params.has('userId')) {
+        path += "&userId=" + params.get('userId');
+    }
+    if (params.has('userName')) {
+        path += "&userName=" + params.get('userName');
+    }
+    if (params.has('region')) {
+        path += '&region=' + params.get('region');
+    }
+    oReq.open("POST", path);
+    oReq.send();
+}
+
+function extractLocale(localeParam) {
+    if (!localeParam) {
+        return defaultLocale;
+    }
+    else if (localeParam === 'autodetect') {
+        return navigator.language;
+    }
+    else {
+        return localeParam;
+    }
+}
+
+function chatRequested() {
+    // const params = new URLSearchParams(location.search);
+    if (params.has('shareLocation')) {
+        getUserLocation(requestChatBot);
+    }
+    else {
+        requestChatBot();
+    }
+}
+
+function getUserLocation(callback) {
+    navigator.geolocation.getCurrentPosition(
+        function(position) {
+            console.log(position);
+            var latitude  = position.coords.latitude;
+            var longitude = position.coords.longitude;
+            var location = {
+                lat: latitude,
+                long: longitude
+            }
+            callback(location);
+        },
+        function(error) {
+            // user declined to share location
+            console.log("location error:" + error.message);
+            callback();
+        });
+}
+
+function initBotConversation() {
+    if (this.status >= 400) {
+        alert(this.statusText);
+        return;
+    }
+    // extract the data from the JWT
+    console.log(this.response)
+    const jsonWebToken = this.response;
+    const tokenPayload = JSON.parse(atob(jsonWebToken.split('.')[1]));
+    const user = {
+        id: tokenPayload.userId,
+        name: tokenPayload.userName,
+        locale: tokenPayload.locale
+    };
+    let domain = undefined;
+    if (tokenPayload.directLineURI) {
+        domain =  "https://" +  tokenPayload.directLineURI + "/v3/directline";
+    }
+    var botConnection = window.WebChat.createDirectLine({
+        token: tokenPayload.connectorToken,
+        domain: domain
+    });
+    const styleOptions = {
+        botAvatarImage: 'https://docs.microsoft.com/en-us/azure/bot-service/v4sdk/media/logo_bot.svg?view=azure-bot-service-4.0',
+        // botAvatarInitials: '',
+        // userAvatarImage: '',
+        hideSendBox: true, /* set to true to hide the send box from the view */
+        botAvatarInitials: 'Bot',
+        userAvatarInitials: 'You',
+        backgroundColor: '#F8F8F8'
+    };
+
+    const store = window.WebChat.createStore({}, function(store) { return function(next) { return function(action) {
+        if (action.type === 'DIRECT_LINE/CONNECT_FULFILLED') {
+            store.dispatch({
+                type: 'DIRECT_LINE/POST_ACTIVITY',
+                meta: {method: 'keyboard'},
+                payload: {
+                    activity: {
+                        type: "invoke",
+                        name: "InitConversation",
+                        locale: user.locale,
+                        value: {
+                            // must use for authenticated conversation.
+                            jsonWebToken: jsonWebToken,
+
+                            // Use the following activity to proactively invoke a bot scenario
+                            triggeredScenario: {
+                                trigger: "c19_entry",
+                                args: {
+                                    region: params.get("region") || null
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+
+        }
+        else if (action.type === 'DIRECT_LINE/INCOMING_ACTIVITY') {
+            if (action.payload && action.payload.activity && action.payload.activity.type === "event" && action.payload.activity.name === "ShareLocationEvent") {
+                // share
+                getUserLocation(function (location) {
+                    store.dispatch({
+                        type: 'WEB_CHAT/SEND_POST_BACK',
+                        payload: { value: JSON.stringify(location) }
+                    });
+                });
+            }
+        }
+        return next(action);
+    }}});
+    const webchatOptions = {
+        directLine: botConnection,
+        styleOptions: styleOptions,
+        store: store,
+        userID: user.id,
+        username: user.name,
+        locale: user.locale
+    };
+    startChat(user, webchatOptions);
+}
+
+function startChat(user, webchatOptions) {
+    const botContainer = document.getElementById('webchat');
+    window.WebChat.renderWebChat(webchatOptions, botContainer);
+}
---
+++ packages/healthbotapp/server.js
@@ -0,0 +1,120 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+
+require('dotenv').config()
+const crypto = require('crypto')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const rp = require('request-promise')
+const WEBCHAT_SECRET = process.env.WEBCHAT_SECRET
+const DIRECTLINE_ENDPOINT_URI = process.env.DIRECTLINE_ENDPOINT_URI
+const APP_SECRET = process.env.APP_SECRET
+const directLineTokenEp = `https://${
+	DIRECTLINE_ENDPOINT_URI || 'directline.botframework.com'
+}/v3/directline/tokens/generate`
+
+// Initialize the web app instance,
+const app = express()
+app.use(cookieParser())
+
+let options = {}
+// uncomment the line below if you wish to allow only specific domains to embed this page as a frame
+//options = {setHeaders: (res, path, stat) => {res.set('Content-Security-Policy', 'frame-ancestors example.com')}};
+// Indicate which directory static resources
+// (e.g. stylesheets) should be served from.
+app.use(express.static(path.join(__dirname, 'public'), options))
+// begin listening for requests.
+const port = process.env.PORT || 8080
+const region = process.env.REGION || 'Unknown'
+
+app.listen(port, function () {
+	console.log('Express server listening on port ' + port)
+})
+
+function isUserAuthenticated() {
+	// add here the logic to verify the user is authenticated
+	return true
+}
+
+const appConfig = {
+	isHealthy: false,
+	options: {
+		method: 'POST',
+		uri: directLineTokenEp,
+		headers: {
+			Authorization: 'Bearer ' + WEBCHAT_SECRET,
+		},
+		json: true,
+	},
+}
+
+function healthResponse(res, statusCode, message) {
+	res.status(statusCode).send({
+		health: message,
+		region: region,
+	})
+}
+function healthy(res) {
+	healthResponse(res, 200, 'Ok')
+}
+
+function unhealthy(res) {
+	healthResponse(res, 503, 'Unhealthy')
+}
+
+app.get('/health', function (req, res) {
+	if (!appConfig.isHealthy) {
+		rp(appConfig.options)
+			.then((body) => {
+				appConfig.isHealthy = true
+				healthy(res)
+			})
+			.catch((err) => {
+				unhealthy(res)
+			})
+	} else {
+		healthy(res)
+	}
+})
+
+app.post('/chatBot', function (req, res) {
+	if (!isUserAuthenticated()) {
+		res.status(403).send()
+		return
+	}
+	rp(appConfig.options)
+		.then(function (parsedBody) {
+			var userid = req.query.userId || req.cookies.userid
+			if (!userid) {
+				userid = crypto.randomBytes(4).toString('hex')
+				res.cookie('userid', userid)
+			}
+
+			var response = {}
+			response['userId'] = userid
+			response['userName'] = req.query.userName
+			response['locale'] = req.query.locale
+			response['connectorToken'] = parsedBody.token
+
+			/*
+            //Add any additional attributes
+            response['optionalAttributes'] = {age: 33};
+            */
+
+			if (req.query.lat && req.query.long) {
+				response['location'] = { lat: req.query.lat, long: req.query.long }
+			}
+			response['directLineURI'] = DIRECTLINE_ENDPOINT_URI
+			const jwtToken = jwt.sign(response, APP_SECRET)
+			res.send(jwtToken)
+		})
+		.catch(function (err) {
+			appConfig.isHealthy = false
+			res.status(err.statusCode).send()
+			console.log('failed')
+		})
+})
