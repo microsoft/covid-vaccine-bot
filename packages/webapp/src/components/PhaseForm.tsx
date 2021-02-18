103c4cee239368085d59343d6783c6d6f061a912
Batch Mosqueda <batch@genui.com>
2021-02-17 23:57:18 -0800

    wip: added moreinfo to display

--- packages/webapp/src/components/PhaseForm.tsx
+++ packages/webapp/src/components/PhaseForm.tsx
@@ -5,26 +5,142 @@
 import { observer } from 'mobx-react-lite'
 import { IconButton, Dropdown, DirectionalHint, TextField, IDetailsRowProps } from '@fluentui/react'
 import { getAppStore } from '../store/store'
+import { useCallback, useEffect, useState } from 'react'
+import { toProperCase } from '../utils/textUtils'
 
 import './PhaseForm.scss'
-
 export interface PhaseFormProps {
-    selectedState: string
+    selectedState: any
     rowItems: IDetailsRowProps
 }
 
 export default observer(function PhaseForm(props: PhaseFormProps) {
-    const { globalFileData, repoFileData } = getAppStore();
+    const { globalFileData, currentLanguage } = getAppStore();
+    const { selectedState , rowItems } = props;
+    const [ phaseTagItems, setPhaseTagItems] = useState<any[]>([])
+    const [ phaseQualifierItems, setPhaseQualifierItems] = useState<any[]>([])
+    const [ moreInfoText, setMoreInfoText ] = useState<string>()
+
+    useEffect(() => {
+        // state tags first, then global. check if region must be check before state and global.
+        const tempTagKeyList: any[] = [];
+        const tempTags: any[] = [];
+        if (selectedState?.value) {
+            const phases = selectedState.value.vaccination.content.phases as Array<any>;
+            phases.forEach(phase => {
+                phase.qualifications.forEach((qualification: any) => {
+                    let tagKey = qualification.question.split('/')[1].split('.')[0];
+                    let baseQuestionKeys = qualification.question.split('/')
+                    let questionKey = `${baseQuestionKeys[0]}/${baseQuestionKeys[1].split('.')[0]}`
+
+                    if (!tempTagKeyList.includes(tagKey)) {
+                        tempTagKeyList.push(tagKey);
+                        tempTags.push({
+                            key: tagKey,
+                            text: toProperCase(tagKey),
+                            questionKey: questionKey
+                        })
+                    }
+                })
+            })
+        }
+
+        if (globalFileData?.customStrings) {
+            const questionKeys = Object.keys(globalFileData.customStrings.content).filter((k) =>
+				k.includes('eligibility.question')
+			)
+
+            questionKeys.forEach(qualification => {
+                let tagKey = qualification.split('/')[1].split('.')[0];
+                let baseQuestionKeys = qualification.split('/')
+                let questionKey = `${baseQuestionKeys[0]}/${baseQuestionKeys[1].split('.')[0]}`
+
+                if (!tempTagKeyList.includes(tagKey)) {
+                    tempTagKeyList.push(tagKey);
+                    tempTags.push({
+                        key: tagKey,
+                        text: `${toProperCase(tagKey)} (global)`,
+                        questionKey: questionKey
+                    })
+                }
+            })
+        }
+        setPhaseTagItems(tempTags.sort((a, b) => (a.key > b.key) ? 1 : -1));
+
+    },[globalFileData, selectedState, rowItems])
+
+    const onTagChange = useCallback((_event, option) => {
+        // TODO: need to filter between health and healthcare
+        const { questionKey } = option;
+        const tempQualifierList: any[] = []
+        if (questionKey) {
+            // add state level qualifiers
+            Object.entries(selectedState.value.strings.content).forEach(([key, value]:[string, any]) => {
+                if(key.startsWith(questionKey)) {
+                    tempQualifierList.push({
+                        key: key,
+                        text: value[currentLanguage]
+                    })
+                }
+            })
+
+            // add global qualifiers
+            Object.entries(globalFileData.customStrings.content).filter(([key, value]: [string, any]) => {
+                if(key.startsWith(questionKey)) {
+                    tempQualifierList.push({
+                        key: key,
+                        text: value[currentLanguage]
+                    })
+                }
+            })
+            setPhaseQualifierItems(tempQualifierList)
+            setMoreInfoText('')
+        }
+    },[globalFileData, selectedState, currentLanguage])
+
+    const onQualifierChange = useCallback((_event, option) => {
+        //TODO: refactor/check if moreinfo should still be in array
+        const moreInfoKey = option.key.replace('question', 'moreinfo')
+        const tempMoreInfoList: any[] = []
+        if (moreInfoKey) {
+            Object.entries(selectedState.value.strings.content).forEach(([key, value]:[string, any]) => {
+                if(key.startsWith(moreInfoKey)) {
+                    tempMoreInfoList.push({
+                        key: key,
+                        text: value[currentLanguage]
+                    })
+                }
+            })
+
+            Object.entries(globalFileData.customStrings.content).filter(([key, value]: [string, any]) => {
+                if(key.startsWith(moreInfoKey)) {
+                    tempMoreInfoList.push({
+                        key: key,
+                        text: value[currentLanguage]
+                    })
+                }
+            })
+            if (tempMoreInfoList.length > 0){
+                setMoreInfoText(tempMoreInfoList[0].text)
+            }
+        }
+    },[globalFileData, selectedState, currentLanguage])
 
 	return (
         <div className='phaseDetailRow'>
             <div className='mainRow'>
-                <Dropdown options={[]} placeholder='Tag'
+                <Dropdown
+                    options={phaseTagItems}
+                    placeholder='Tag'
                     className='tagDropdown'
-                    styles={{root: {width: 400}}}
+                    styles={{root: {minWidth: 250}}}
+                    onChange={onTagChange}
                 />
-                <Dropdown options={[]} placeholder='Qualifier'
-                    styles={{root: {width: '100%'}}}
+                <Dropdown
+                    options={phaseQualifierItems}
+                    placeholder='Qualifier'
+                    styles={{root: {width: '100%', minWidth: 0}}}
+                    onChange={onQualifierChange}
                 />
                 <IconButton
                     iconProps={{iconName: 'MoreVertical'}}
@@ -54,6 +170,7 @@ export default observer(function PhaseForm(props: PhaseFormProps) {
                     autoAdjustHeight={true}
                     resizable={false}
                     styles={{root: {width: 'calc(100% - 32px)', padding: '5px 0'}}}
+                    defaultValue={moreInfoText}
                 />
                 <TextField
                     placeholder='More info url'
