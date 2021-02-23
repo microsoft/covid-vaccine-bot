536f9180f3834c7eb76d91ede4e8912b30d33308
Batch Mosqueda <batch@genui.com>
2021-02-22 17:25:27 -0800

    lint fixes

--- packages/webapp/src/components/LocationForm.tsx
+++ packages/webapp/src/components/LocationForm.tsx
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { observer } from 'mobx-react-lite'
 import {
 	PrimaryButton,
     DefaultButton,
@@ -10,6 +9,7 @@ import {
     Dropdown,
     IDropdownOption
 } from '@fluentui/react'
+import { observer } from 'mobx-react-lite'
 import { useCallback, useRef, useState } from 'react'
 import { getCustomStrings } from '../selectors/locationSelectors'
 
