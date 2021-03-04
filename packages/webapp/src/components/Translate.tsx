b0888ae491f64945c5cc92cb3444dd58b4befbd2
Batch Mosqueda <batch@genui.com>
2021-03-03 18:32:14 -0800

    revised translate component functions

--- packages/webapp/src/components/Translate.tsx
+++ packages/webapp/src/components/Translate.tsx
@@ -27,16 +27,8 @@ const filterDropdownStyles = {
 }
 
 export default observer(function Translate() {
-	const [locationList, setLocationList] = useState<any[]>([])
-	const [qualifierList, setQualifierList] = useState<any[]>([])
-	const { globalFileData, repoFileData,currentLanguage } = getAppStore()
+	const { globalFileData, repoFileData, currentLanguage } = getAppStore()
 	const languageOptions = getLanguageOptions()
-	const [translateLanguage, setTranslateLanguage]= useState<any>(languageOptions[0])
-	const locationChanges = useRef<any>({})
-	const qualifierChanges = useRef<any>({})
-	const mainLanguage = useRef<string>(currentLanguage)
-	const locationFullList = useRef<any[]>([])
-	const qualifierFullList = useRef<any[]>([])
 	const translationFilter = [
 		{
 			key: 'missing',
@@ -47,10 +39,20 @@ export default observer(function Translate() {
 			text: 'Show All'
 		}
 	]
-	const translationFilterState = useRef<any>(translationFilter[0].key)
+
+	const locationChanges = useRef<any>({})
+	const qualifierChanges = useRef<any>({})
+	const mainLanguage = useRef<string>(currentLanguage)
+	const locationFullList = useRef<any[]>([])
+	const qualifierFullList = useRef<any[]>([])
+	const toLanguage = useRef<any>(languageOptions[0].key)
+
+	const [locationList, setLocationList] = useState<any[]>([])
+	const [qualifierList, setQualifierList] = useState<any[]>([])
+	const [translationFilterState, setTranslationFilterState] = useState<any>(translationFilter[0].key)
 	const [isSectionCollapse, setSectionCollapse] = useState<any>({locations: false, qualifiers: false, info: false})
 
-	const buildLocationList = useCallback(() => {
+	const buildTranslationsLists = useCallback(() => {
 		if (repoFileData) {
 			const tempList: any[] = []
 			Object.entries(repoFileData).forEach(([_key, value]: [string, any]) => {
@@ -64,36 +66,25 @@ export default observer(function Translate() {
 						? stateNames[`cdc/${stateId}/state_name`][mainLanguage.current]
 						: `*Translation Not Found* (${stateId})`
 
-				const translateToValue = !translateLanguage.key
+				const translateToValue = !toLanguage.current
 					? ''
-					: !stateNames?.[`cdc/${stateId}/state_name`]?.[translateLanguage.key]
+					: !stateNames?.[`cdc/${stateId}/state_name`]?.[toLanguage.current]
 						? ''
-						: stateNames?.[`cdc/${stateId}/state_name`]?.[translateLanguage.key]
+						: stateNames?.[`cdc/${stateId}/state_name`]?.[toLanguage.current]
 
 				tempList.push({
 					key: `cdc/${stateId}/state_name`,
 					fromKey: mainLanguage.current,
 					from: stateLabel,
-					toKey: translateLanguage.key,
+					toKey: toLanguage.current,
 					to: translateToValue,
 					_to: translateToValue
 				})
 			})
 			const locationNames = tempList.sort((a,b) => (a.from > b.from) ? 1 : -1)
-
-			if (translationFilterState.current === 'missing') {
-				setLocationList(
-					locationNames.filter(location => !location._to)
-				)
-			} else {
-				setLocationList(locationNames)
-			}
-
 			locationFullList.current = locationNames
 		}
-	},[globalFileData, repoFileData, translateLanguage, mainLanguage, locationFullList, translationFilterState, setLocationList])
 
-	const buildQualifierList = useCallback(() => {
 		if (globalFileData) {
 			const tempList: any[] = []
 			Object.entries(globalFileData.customStrings.content).forEach(([key, value]: [string, any]) => {
@@ -102,42 +93,21 @@ export default observer(function Translate() {
 						key: key,
 						fromKey: mainLanguage.current,
 						from: value[mainLanguage.current],
-						toKey: translateLanguage.key,
-						to: value[translateLanguage.key],
-						_to: value[translateLanguage.key]
+						toKey: toLanguage.current,
+						to: value[toLanguage.current],
+						_to: value[toLanguage.current]
 					})
 				}
 			})
 			const qualifiers = tempList.sort((a,b) => (a.from > b.from) ? 1 : -1)
-
-			if (translationFilterState.current === 'missing') {
-				setQualifierList(
-					qualifiers.filter(qualifier => !qualifier._to)
-				)
-			} else {
-				setQualifierList(qualifiers)
-			}
-
 			qualifierFullList.current = qualifiers
 		}
-	},[globalFileData, translateLanguage, mainLanguage, qualifierFullList, translationFilterState, setQualifierList])
+	},[globalFileData, repoFileData])
 
 	useEffect(() => {
 		mainLanguage.current = currentLanguage
-		setTranslateLanguage(getLanguageOptions()[0])
-	},[currentLanguage, mainLanguage, setTranslateLanguage])
-
-	useEffect(() => {
-		buildLocationList()
-		buildQualifierList()
-	},[translationFilterState, translateLanguage, mainLanguage, buildLocationList, buildQualifierList])
-
-	const onTranslateLanguageChange = useCallback(
-		(_ev, option) => {
-			setTranslateLanguage(option)
-		},
-		[setTranslateLanguage]
-	)
+		buildTranslationsLists()
+	},[currentLanguage, mainLanguage, buildTranslationsLists])
 
 	const onCollapseSection = useCallback((name: string) => {
 		setSectionCollapse({...isSectionCollapse, ...{[name]: !isSectionCollapse[name]}})
@@ -173,7 +143,7 @@ export default observer(function Translate() {
 
 	const updateLocationTranslation = useCallback((item) => {
 		if (item.to && (item.to.toLowerCase() !== item._to.toLowerCase())) {
-			item.to = toProperCase(item.to)
+			item.to = toProperCase(item.to).trim()
 			translateLocationName(item)
 		}
 	},[])
@@ -181,12 +151,30 @@ export default observer(function Translate() {
 	const updateQualifierTranslation = useCallback((item) => {
 		if (item.to && (item.to.toLowerCase() !== item._to.toLowerCase())) {
 			item.to = String(item.to).trim()
-			console.log(item)
 			translateQualifier(item)
 		}
 	},[])
 
-	const onTranslateFilterChange = useCallback((_ev, option) => {
+	const onLanguageChange = useCallback(
+		(_ev, option) => {
+			toLanguage.current = option.key
+			buildTranslationsLists()
+			if (translationFilterState === 'missing') {
+				setLocationList(
+					locationFullList.current.filter(i => !i._to)
+				)
+				setQualifierList(
+					qualifierFullList.current.filter(i => !i._to)
+				)
+			} else {
+				setLocationList(locationFullList.current)
+				setQualifierList(qualifierFullList.current)
+			}
+		},
+		[translationFilterState, buildTranslationsLists, setLocationList, setQualifierList]
+	)
+
+	const onTranslationFilterChange = useCallback((_ev, option) => {
 		if (option.key === 'missing') {
 			setLocationList(
 				locationFullList.current.filter(i => !i._to)
@@ -199,8 +187,8 @@ export default observer(function Translate() {
 			setQualifierList(qualifierFullList.current)
 		}
 
-		translationFilterState.current = option.key
-	},[setLocationList, setQualifierList, locationFullList, qualifierFullList, translationFilterState])
+		setTranslationFilterState(option.key)
+	},[setLocationList, setQualifierList, locationFullList, qualifierFullList, setTranslationFilterState])
 
 	return (
 		<div className="translatePageContainer">
@@ -214,18 +202,18 @@ export default observer(function Translate() {
 				<div className="bodyContent">
 					<div className="filterGroup">
 						<Dropdown
-							selectedKey={translateLanguage.key}
+							selectedKey={toLanguage.current}
 							placeholder="Available languages"
 							options={languageOptions}
 							styles={filterDropdownStyles}
-							onChange={onTranslateLanguageChange}
+							onChange={onLanguageChange}
 						/>
 						<Dropdown
-							selectedKey={translationFilterState.current}
+							selectedKey={translationFilterState}
 							placeholder="Needs Translation"
 							options={translationFilter}
 							styles={filterDropdownStyles}
-							onChange={onTranslateFilterChange}
+							onChange={onTranslationFilterChange}
 						/>
 					</div>
 					<section>
@@ -253,7 +241,7 @@ export default observer(function Translate() {
 									)
 								})
 							):(
-								<div className="emptyTranslateListRow">No missing location translations found for: {getLanguageDisplayText(translateLanguage.key, translateLanguage.key)}.</div>
+								<div className="emptyTranslateListRow">No missing location translations found for: {getLanguageDisplayText(toLanguage.current, toLanguage.current)}.</div>
 							)
 						)}
 					</section>
@@ -285,7 +273,7 @@ export default observer(function Translate() {
 									)
 								})
 							):(
-								<div className="emptyTranslateListRow">No missing qualifier translations found for: {getLanguageDisplayText(translateLanguage.key, translateLanguage.key)}.</div>
+								<div className="emptyTranslateListRow">No missing qualifier translations found for: {getLanguageDisplayText(toLanguage.current, toLanguage.current)}.</div>
 							)
 						)}
 					</section>
