1e04d99fcbe33df2d163b28bd945a6ccd99261a5
Chris Trevino <chtrevin@microsoft.com>
2021-03-16 00:06:52 -0700

    add review document generation

---
+++ packages/plans/scripts/generateReviewDocument/ReviewDocument.ts
@@ -0,0 +1,122 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import { Link, Region, RolloutPhase } from '@covid-vax-bot/state-plan-schema'
+
+interface Section {
+	level: number
+	title: string
+	text?: string
+}
+export class ReviewDocument {
+	public constructor(
+		private localization: Map<string, string>,
+		private policies: Region[]
+	) {}
+
+	public generate(): string {
+		return this.policies.map((r) => this.printRegion(r)).join('\n\n')
+	}
+
+	private printRegion(region: Region, prefix = '', level = 1): string {
+		const links = region.plan?.links || ({} as Record<string, Link>)
+		const phases: RolloutPhase[] = region.plan?.phases || []
+		const subregions = region.regions || []
+		const activePhase = region.plan?.activePhase
+		const phase = phases.find((p) => p.id === activePhase)
+		const name = `${prefix}${region.name}`
+
+		const regionName: Section = {
+			level,
+			title: name,
+		}
+		const linksSection: Section = {
+			level: level + 1,
+			title: name,
+			text: this.printLinkTable(links),
+		}
+		const phaseSection: Section | null =
+			!activePhase && phase == null
+				? null
+				: {
+						level: level + 1,
+						title: `${name} Active Phase (${activePhase})`,
+						text: this.printPhase(phase!),
+				  }
+
+		const subregionSection: Section | null =
+			subregions.length === 0
+				? null
+				: {
+						level: level + 1,
+						title: `${name} Regions`,
+						text: region.regions
+							?.map((r) => this.printRegion(r, `${name}::`, level + 1))
+							.join('\n\n'),
+				  }
+
+		return [regionName, linksSection, phaseSection, subregionSection]
+			.filter((t) => !!t)
+			.map((sec) => this.printSection(sec as Section))
+			.join('\n\n')
+	}
+
+	private printLinkTable(links: Record<string, Link>): string {
+		if (links == null) {
+			return `none`
+		}
+		const rows = Object.keys(links).map((linkKey) => {
+			const link = links[linkKey]
+			const linkText = (link.text || '').trim()
+			const linkTextToShow =
+				linkText.length > 0
+					? this.localization.get(linkText.toLowerCase())
+					: link.url
+			return `| ${linkKey} | [${linkTextToShow}](${link.url}) |`
+		})
+
+		return `| link type | link |
+| --- | --- |
+${rows.join('\n')}
+`
+	}
+
+	private printPhase(phase: RolloutPhase | undefined): string {
+		if (phase == null) {
+			return 'none'
+		}
+		const qualifications = phase.qualifications.map(
+			({ question, moreInfoText: moreInfoTextId, moreInfoUrl }) => {
+				const questionText = this.localization.get(question.toLowerCase())
+				const moreInfoText = moreInfoTextId
+					? this.localization.get(moreInfoTextId.toLowerCase())
+					: ''
+
+				let result = `* ${questionText}`
+
+				if (moreInfoText) {
+					result += `\n\t* ${moreInfoText}`
+				}
+				if (moreInfoUrl) {
+					result += ` (see [here](${moreInfoUrl}))`
+				}
+				return result
+			}
+		)
+		return `${qualifications.join('\n')}`
+	}
+
+	private printSection(section: Section): string {
+		let heading = ''
+		for (let i = 0; i < section.level; ++i) {
+			heading += '#'
+		}
+
+		let result = `${heading} ${section.title}`
+		if (section.text) {
+			result += '\n\n' + section.text
+		}
+		return result
+	}
+}
---
+++ packages/plans/scripts/generateReviewDocument/index.ts
@@ -0,0 +1,38 @@
+/*!
+ * Copyright (c) Microsoft. All rights reserved.
+ * Licensed under the MIT license. See LICENSE file in the project.
+ */
+import fs from 'fs'
+import { join } from 'path'
+import { DIST_DIR } from '../createDistDir'
+import { readCsvFile } from '../readCsvFile'
+import { ReviewDocument } from './ReviewDocument'
+import { Region } from '@covid-vax-bot/state-plan-schema'
+
+async function generateReviewDocument(): Promise<void> {
+	const OUTPUT_FILE_NAME = join(DIST_DIR, 'policies.md')
+	const policies = readPolicies()
+	const loc = readLocalizationTable()
+
+	const result = new ReviewDocument(loc, policies).generate()
+
+	fs.writeFileSync(OUTPUT_FILE_NAME, result)
+}
+
+function readLocalizationTable(): Map<string, string> {
+	const LOCALIZATION_FILE = join(DIST_DIR, 'localization.csv')
+	const records: Record<string, string>[] = []
+	readCsvFile(LOCALIZATION_FILE, records)
+
+	const result = new Map<string, string>()
+	records.forEach((r) => result.set(r['String ID'], r['en-us']))
+	return result
+}
+
+function readPolicies(): Region[] {
+	return require(join(DIST_DIR, 'policies.json'))
+}
+
+generateReviewDocument()
+	.then(() => console.log('generated policies.md'))
+	.catch((err) => console.error('error generating policies.md', err))
