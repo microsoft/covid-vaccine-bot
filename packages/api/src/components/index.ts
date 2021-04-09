0a689d53d3d0f10f4ebd75ec3cac043643c5e12b
Chris Trevino <chtrevin@microsoft.com>
2021-04-09 13:09:06 -0700

    remove eligibility API components

--- packages/api/src/components/index.ts
+++ packages/api/src/components/index.ts
@@ -2,13 +2,9 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { Localizer } from './Localizer'
-import { Locator } from './Locator'
 import { ProviderLocationsStore } from './ProviderLocationsStore'
 import { getCosmosContainer } from './cosmos'
 
-export const locator = new Locator()
-export const localizer = new Localizer()
 export const providerLocationsStore = new ProviderLocationsStore(
 	getCosmosContainer()
 )
