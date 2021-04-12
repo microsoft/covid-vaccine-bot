e667574db097dede80fca7b8017ebda91c481ac1
Devan Huapaya <devan@genui.com>
2021-04-12 13:57:25 -0700

    Cleaned lint warning

--- packages/webapp/src/components/Footer.tsx
+++ packages/webapp/src/components/Footer.tsx
@@ -2,8 +2,8 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { FC, memo } from 'react'
 import { observer } from 'mobx-react-lite'
+import { FC, memo } from 'react'
 import { getText as t } from '../selectors/intlSelectors'
 
 const CONTACT_EMAIL = process.env.REACT_APP_CONTACT_US_EMAIL
@@ -17,7 +17,9 @@ export default observer(function Footer() {
 					{t('App.Footer.privacyCookies')}
 				</Link>
 				{' | '}
-				<Link href={`mailto:${CONTACT_EMAIL}`}>{t('App.Footer.contactUs')}</Link>
+				<Link href={`mailto:${CONTACT_EMAIL}`}>
+					{t('App.Footer.contactUs')}
+				</Link>
 				{' | '}
 				<Link href={CODE_OF_CONDUCT_URL}>{t('App.Footer.codeOfConduct')}</Link>
 			</div>
