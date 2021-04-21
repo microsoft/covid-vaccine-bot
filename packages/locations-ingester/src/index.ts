eaaa6e185fb81dee5553cc9582d0f6725ca95002
Chris Trevino <chtrevin@microsoft.com>
2021-04-20 23:23:33 -0700

    update index

--- packages/locations-ingester/src/index.ts
+++ packages/locations-ingester/src/index.ts
@@ -2,23 +2,13 @@
  * Copyright (c) Microsoft. All rights reserved.
  * Licensed under the MIT license. See LICENSE file in the project.
  */
-import { fetchS3Data } from './fetchS3Data/fetchS3Data'
-import { geocodeData } from './geocodeData/geocodeData'
-import { getLatestFilePath } from './io'
-import { transformData } from './transformData/transformData'
-import { writeCosmosData } from './writeCosmosData/writeCosmosData'
+import { ingest } from './ingest'
+import { Context } from '@azure/functions'
 
-export default async function ingest() {
-	try {
-		console.log('fetching S3 Data')
-		await fetchS3Data()
-		console.log('transforming data to JSON')
-		await transformData(getLatestFilePath())
-		console.log('geocoding data')
-		await geocodeData()
-		console.log('writing data to cosmosdb')
-		await writeCosmosData()
-	} catch (err) {
-		console.error(`error ingesting data`, err)
-	}
+function scheduledIngest(context: Context) {
+	ingest()
+		.then(() => context.done())
+		.catch((err) => context.done(err))
 }
+
+export default scheduledIngest
